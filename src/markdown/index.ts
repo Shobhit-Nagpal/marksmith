f12bd5e363a5447c71ad1b794375fe112125310f
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-05-06 03:28:18 +0530

    add markdown classes

--- src/markdown/index.ts
+++ src/markdown/index.ts
@@ -1,1 +1,45 @@
-console.log("markdown");
+export enum Markdown {
+  TEXT = "text",
+  BOLD = "bold",
+  ITALIC = "italic",
+  CODE = "code",
+  LINK = "link",
+  IMAGE = "image",
+}
+
+export class MarkdownContent {
+  text: string;
+  url: string;
+
+  constructor(text: string = "", url: string = "") {
+    this.text = text;
+    this.url = url;
+  }
+
+  isEqual(md: MarkdownContent): boolean {
+    if (this.text === md.text && this.url === md.url) {
+      return true;
+    }
+    return false;
+  }
+}
+
+export class MarkdownImage extends MarkdownContent {
+  constructor(text: string, url: string) {
+    super(text, url);
+  }
+
+  toString(): string {
+    return `MarkdownImage(${this.text}, ${this.url})`;
+  }
+}
+
+export class MarkdownLink extends MarkdownContent {
+  constructor(text: string, url: string) {
+    super(text, url);
+  }
+
+  toString(): string {
+    return `MarkdownImage(${this.text}, ${this.url})`;
+  }
+}
