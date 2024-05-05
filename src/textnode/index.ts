d9acd8b299c7eebc539c084b396657f7683a465f
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-05-06 00:01:43 +0530

    add text node + unit tests

--- src/textnode/index.ts
+++ src/textnode/index.ts
@@ -1,1 +1,22 @@
-console.log("textnode");
+export class TextNode {
+  text: string;
+  textType: string;
+  url: string;
+
+  constructor(text: string = "", textType: string = "", url: string = "") {
+    this.text = text;
+    this.textType = textType;
+    this.url = url;
+  }
+
+  toString(): string {
+    return `TextNode(${this.text}, ${this.textType}, ${this.url})`
+  }
+
+  isEqual(textNode: TextNode): boolean {
+    if (this.text === textNode.text && this.textType === textNode.textType && this.url === textNode.url) {
+      return true;
+    } 
+    return false;
+  }
+}
--- tests/textnode.test.ts
+++ tests/textnode.test.ts
@@ -1,1 +1,33 @@
-console.log("textnode test");
+import { TextNode } from "../src/textnode";
+
+test("text nodes are equal", () => {
+  const tn1 = new TextNode("This is a text", "bold");
+  const tn2 = new TextNode("This is a text", "bold");
+  const equal = tn1.isEqual(tn2);
+  expect(equal).toBe(true);
+});
+
+test("text nodes are not equal", () => {
+  const tn1 = new TextNode("This is a text", "bold");
+  const tn2 = new TextNode("This is a text", "italic");
+  const equal = tn1.isEqual(tn2);
+  expect(equal).toBe(false);
+});
+
+test("text nodes are equal with url", () => {
+  const tn1 = new TextNode("This is a text", "bold", "https://example.com/");
+  const tn2 = new TextNode("This is a text", "italic", "https://example.com/");
+  const equal = tn1.isEqual(tn2);
+  expect(equal).toBe(false);
+});
+
+test("text nodes are not equal with url", () => {
+  const tn1 = new TextNode("This is a text", "bold", "https://example.com/");
+  const tn2 = new TextNode(
+    "This is a text",
+    "italic",
+    "https://different.example.com/",
+  );
+  const equal = tn1.isEqual(tn2);
+  expect(equal).toBe(false);
+});
